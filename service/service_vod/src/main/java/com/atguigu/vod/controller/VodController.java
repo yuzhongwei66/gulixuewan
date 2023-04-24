b2e0ec72237b281238851aadba7080cba1f840ab
itcast <1176922450@qq.com>
2023-04-24 19:59:55 +0800

    .

--- service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
@@ -1,11 +1,21 @@
 package com.atguigu.vod.controller;
 
+import com.aliyun.oss.ClientException;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoResponse;
 import com.atguigu.commonutils.R;
-
+import com.atguigu.servicebase.exceptionhandler.GuliException;
+import com.atguigu.vod.Utils.ConstantVodUtils;
+import com.atguigu.vod.Utils.InitVodCilent;
 import com.atguigu.vod.service.VodService;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/eduvod/video")
@@ -22,6 +32,31 @@ public class VodController {
         String videoId = vodService.uploadVideoAly(file);
         return R.ok().data("videoId",videoId);
     }
-    @DeleteMapping("removeAlyiVideo/{id}")
-    public R
+
+    //根据视频id删除阿里云视频
+    @DeleteMapping("removeAlyVideo/{id}")
+    public R removeAlyVideo(@PathVariable String id) {
+        try {
+            //初始化对象
+            DefaultAcsClient client = InitVodCilent.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            //创建删除视频request对象
+            DeleteVideoRequest request = new DeleteVideoRequest();
+            //向request设置视频id
+            request.setVideoIds(id);
+            //调用初始化对象的方法实现删除
+            client.getAcsResponse(request);
+            return R.ok();
+        }catch(Exception e) {
+            e.printStackTrace();
+            throw new GuliException(20001,"删除视频失败");
+        }
+    }
+
+    //删除多个阿里云视频的方法
+    //参数多个视频id  List videoIdList
+    @DeleteMapping("delete-batch")
+    public R deleteBatch(@RequestParam("videoIdList") List<String> videoIdList) {
+        vodService.removeMoreAlyVideo(videoIdList);
+        return R.ok();
+    }
 }
--- service/service_vod/src/main/java/com/atguigu/vod/service/impl/VodServiceImpl.java
+++ service/service_vod/src/main/java/com/atguigu/vod/service/impl/VodServiceImpl.java
@@ -3,13 +3,20 @@ package com.atguigu.vod.service.impl;
 import com.aliyun.vod.upload.impl.UploadVideoImpl;
 import com.aliyun.vod.upload.req.UploadStreamRequest;
 import com.aliyun.vod.upload.resp.UploadStreamResponse;
-
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
+import com.atguigu.commonutils.R;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
 import com.atguigu.vod.Utils.ConstantVodUtils;
+import com.atguigu.vod.Utils.InitVodCilent;
 import com.atguigu.vod.service.VodService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VodServiceImpl implements VodService {
@@ -44,5 +51,36 @@ public class VodServiceImpl implements VodService {
             return null;
         }
 
+    }
+
+    @Override
+    public void removeMoreAlyVideo(List videoIdList) {
+        try {
+            //初始化对象
+            DefaultAcsClient client = InitVodCilent.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            //创建删除视频request对象
+            DeleteVideoRequest request = new DeleteVideoRequest();
+
+            //videoIdList值转换成 1,2,3
+            String videoIds = StringUtils.join(videoIdList.toArray(), ",");
+
+            //向request设置视频id
+            request.setVideoIds(videoIds);
+            //调用初始化对象的方法实现删除
+            client.getAcsResponse(request);
+        }catch(Exception e) {
+            e.printStackTrace();
+            throw new GuliException(20001,"删除视频失败");
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("11");
+        list.add("22");
+        list.add("33");
+        // 11,22,33
+        String join = StringUtils.join(list.toArray(), ",");
+        System.out.println(join);
     }
 }
